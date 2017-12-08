347df154d9108e2b5556caef91cfa1837dbefd9f
Fabrício Mazzola <fabriciommazzola@gmail.com>
2017-12-08 20:14:15 -0200

    Alteracoes nas restrições :no_entry_sign:

--- instanceToDat.cpp
+++ instanceToDat.cpp
@@ -41,47 +41,23 @@ int main(){
 		}
 	}
 
-  cout << "data;" << endl << "set N:= ";
+  // Output "data" info
+  cout << "data;" << endl << "param n:= " << dimension << endl;
 
+  cout << "set V:= ";
+  for (size_t i = 0; i < dimension; i++) {
+    cout << i << " ";
+  }
+	cout << endl;
 
-
-
+  cout << ";" << endl << "set E:= ";
+  for (size_t i = 1; i < dimension; i++) { // TODO verificar se ta certo esses valores e a variavel
+    cout << i << " ";
+  }
 
 	/*
-
-  Cada linha possui N valores inteiros separados por espaço. Dada uma linha i
-  # qualquer, e uma coluna j qualquer, se o valor v presente na posição (i,j) for
-  # tal que v >=0 , então v é a distância entre os vértices i e j no grafo. Caso
-  # v = -1, então quer dizer que o vértice i só pode ser visitado após o vértice j
-  # e NÃO antes. Em outras palavras, o vértice j deve preceder i na solução.
-  #
-
-
-
-	NAME: ESC07.sop
-	TYPE: SOP
-	COMMENT: Received by Norbert Ascheuer / Laureano Escudero
-	DIMENSION: 9
-	EDGE_WEIGHT_TYPE: EXPLICIT
-	EDGE_WEIGHT_FORMAT: FULL_MATRIX
-	EDGE_WEIGHT_SECTION
-	0    0    0    0    0    0    0    0 1000000
-	-1    0  100  200   75    0  300  100    0
-	-1  400    0  500  325  400  600    0    0
-	-1  700  800    0  550  700  900  800    0
-	-1   -1  250  225    0  275  525  250    0
-	-1   -1  100  200   -1    0   -1   -1    0
-	-1   -1 1100 1200 1075 1000    0 1100    0
-	-1   -1    0  500  325  400  600    0    0
-	-1   -1   -1   -1   -1   -1   -1   -1    0
-	EOF
-
-
-	*/
-
-
-
-
+  cout << ";" << endl << "param INF:= 1000000;" << endl << "param c:" << endl;
+  */
 
 	return 0;
 }
